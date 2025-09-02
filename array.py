aa44b64239fd4a2157a05f6147d7351ea5df204f
mirellyslvfarias <mirellyslvv@gmail.com>
2025-09-01 21:09:07 -0300

    Create array.py

---
+++ array.py
@@ -0,0 +1,13 @@
+# Listas com 5 nomes de colegas de turma
+colegas = ["Caroline", "Mirelly", "Suzane", "Vinicius","Marcos"]
+
+# Mostrando elementos usando indices
+print("Primeiro elemento:", colegas[0])
+print("Terceiro elemento:", colegas[1])
+print("Ultimo elemento:", colegas[-1])
+
+# Desafio extra com IF
+if "Mirelly" in colegas:
+    print("Mirelly está na lista.")
+else:
+    print("Mirelly não foi encontrada na lista.")
